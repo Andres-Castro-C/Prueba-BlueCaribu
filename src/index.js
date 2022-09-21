2707d8f2f61dc0c4f996bba7f47244b11079f227
Andres-Castro-C <est.william.castro1@unimilitar.edu.co>
2022-09-20 23:26:18 -0500

    Terminado de cartas dinamicas con Js y menu Flotante de iconos

--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
 console.log("Estoy Funcionado");
 
+// Variables de iconos para hacer el llamado del click 
 const icono_persona = document.querySelector(".icono_persona");
 const icono_empresa = document.querySelector(".icono_empresa");
 const icono_seguridad = document.querySelector('.icono_seguridad');
 const icono_seting = document.querySelector('.icono_seting');
 const icono_folder = document.querySelector('.icono_folder');
 const icono_tiempo = document.querySelector('.icono_tiempo');
-const button_google = document.querySelector('.button-google'); 
+const button_google = document.querySelector('.button-google');
 
+// VARibles de Esferita arriba del menu
+const esferita = document.querySelector(".esferita");
+const esferita2 = document.querySelector('.esferita2');
+const esferita3 = document.querySelector('.esferita3');
+const esferita4 = document.querySelector('.esferita4');
+const esferita5 = document.querySelector('.esferita5');
+const esferita6 = document.querySelector('.esferita6');
+
+//Llamamos a la clase cartas para poder interactutar con el none
 const card_personas = document.querySelector(".card-persona");
 const card_empresa = document.querySelector(".card-empresa");
 const card_seguimiento = document.querySelector('.card-seguimiento');
@@ -16,8 +26,10 @@ const card_notas = document.querySelector('.card-notas');
 const card_tiempo = document.querySelector('.card-tiempo');
 const card_google = document.querySelector('.card-google');
 
+const flota_contacto = document.querySelector('.m1');
+const flota_empresa = document.querySelector('.m2')
 
-
+//Llamado del envento click
 icono_persona.addEventListener('click', verCartaPersona);
 icono_empresa.addEventListener('click', verCartaEmpresa);
 icono_seguridad.addEventListener('click', verCartaSeguridad);
@@ -26,33 +38,44 @@ icono_folder.addEventListener('click', verCartaNotas);
 icono_tiempo.addEventListener('click', verCartaTiempo);
 button_google.addEventListener('click', verCartaGoogle);
 
-function verCartaEmpresa(){
-    card_empresa.classList.toggle('invisible-empresa');
+//Evento de pasar el mause para que flote una alerta
+icono_persona.addEventListener('mouseenter', contactoFLota)
+icono_empresa.addEventListener('mouseenter', contactoFLota)
+
+//Funciones del click 
+function verCartaPersona() {
+    card_personas.classList.toggle('invisible-persona');
+    esferita.classList.toggle('mostar-icono')
     console.log("redy");
 }
 
-function verCartaPersona() {
-    card_personas.classList.toggle('invisible-persona');
+function verCartaEmpresa(){
+    card_empresa.classList.toggle('invisible-empresa');
+    esferita2.classList.toggle('mostar-icono2')
     console.log("redy");
 }
 
 function verCartaSeguridad(){
     card_seguimiento.classList.toggle('invisible-seguimiento');
+    esferita3.classList.toggle('mostar-icono3')
     console.log('redy');
 }
 
 function verCartaSeting(){
     card_seting.classList.toggle('invisible-personalizacion');
+    esferita4.classList.toggle('mostar-icono4')
     console.log('redy');
 }
 
 function verCartaNotas(){
     card_notas.classList.toggle('invisible-Notas');
+    esferita5.classList.toggle('mostar-icono5')
     console.log('redy');
 }
 
 function verCartaTiempo(){
     card_tiempo.classList.toggle('invisible-tiempo');
+    esferita6.classList.toggle('mostar-icono6')
     console.log('redy');
 }
 
@@ -60,3 +83,11 @@ function verCartaGoogle(){
     card_google.classList.toggle('invisible-GoogleAds');
     console.log('redy');
 }
+
+//Funciones de mouse
+function contactoFLota(){
+    flota_contacto.classList.toggle('flotante');
+    flota_empresa.classList.toggle('flotante2')
+    console.log('flota');
+}
+
